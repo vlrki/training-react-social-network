21a499c135a31af4818008c16d4911a7abdced71
vlrki <valera.kirichenko@gmail.com>
2020-02-27 01:08:43 +0500

    redux-form

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -5,12 +5,14 @@ import usersReducer from "./users-reducer";
 import authReducer from "./auth-reducer";
 import thunkMiddleware from "redux-thunk";
 import { applyMiddleware } from "redux";
+import { reducer as formReducer } from 'redux-form';
 
 let reducers = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     usersPage: usersReducer,
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 });
 
 let store = createStore(reducers, applyMiddleware(thunkMiddleware));
