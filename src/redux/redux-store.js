abbf45551f8686e669bd9815748fdd8e127e7835
vlrki <valera.kirichenko@gmail.com>
2020-03-08 19:23:03 +0500

    Redux DevTools

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,10 +1,10 @@
-import { combineReducers, createStore } from "redux";
+
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import profileReducer from "./profile-reducer";
 import dialogsReducer from "./dialogs-reducer";
 import usersReducer from "./users-reducer";
 import authReducer from "./auth-reducer";
 import thunkMiddleware from "redux-thunk";
-import { applyMiddleware } from "redux";
 import { reducer as formReducer } from 'redux-form';
 import appReducer from "./app-reducer";
 
@@ -17,6 +17,12 @@ let reducers = combineReducers({
     app: appReducer
 });
 
-let store = createStore(reducers, applyMiddleware(thunkMiddleware));
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, /* preloadedState, */ composeEnhancers(
+    applyMiddleware(thunkMiddleware)
+));
+
+window.__store__ = store;
 
 export default store;
