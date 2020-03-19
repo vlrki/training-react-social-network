b157cdd679aaefbdc5cb6bcf7b618bf27cbabb75
vlrki <valera.kirichenko@gmail.com>
2020-03-20 01:52:35 +0500

    Image uploading

--- src/api/userAPI.js
+++ src/api/userAPI.js
@@ -45,6 +45,17 @@ export const profileAPI = {
 
     putStatus(status) {
         return instance.put('profile/status', { status: status });
+    },
+
+    savePhoto(photoFile) {
+        var formData = new FormData();
+        formData.append("image", photoFile);
+
+        return instance.put('profile/photo', formData, { 
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        });
     }
 };
 
--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Profile from './Profile';
-import { addPost, getUserProfile, getStatus, updateStatus } from "../../redux/profile-reducer";
+import { addPost, getUserProfile, getStatus, updateStatus, savePhoto } from "../../redux/profile-reducer";
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { withRouter, Redirect } from 'react-router-dom';
@@ -9,23 +9,38 @@ import { withAuthRedirect } from '../../hoc/withAuthRedirect';
 
 class ProfileContainer extends React.Component {
 
-    componentDidMount() {
-
+    refreshProfile() {
         let userId = this.props.match.params.userId;
 
-        if (!userId && this.props.isAuth) {
-            userId = 5851 //this.props.authorizedUserId; //;
-        } else {
-            // <Redirect to="login" />
+        if (!userId) {
+            userId = this.props.authorizedUserId;
+
+            if (!userId) {
+                this.props.history.push("/login");
+            }
         }
 
         this.props.getUserProfile(userId);
         this.props.getStatus(userId);
     }
 
+    componentDidMount() {
+        this.refreshProfile();
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (this.props.match.params.userId != prevProps.match.params.userId) {
+            this.refreshProfile();
+        }
+    }
+
     render() {
         return (
-            <Profile {...this.props} profile={this.props.profile} updateStatus={this.props.updateStatus} />
+            <Profile {...this.props} 
+            isOwner={!this.props.match.params.userId}
+            profile={this.props.profile} 
+            updateStatus={this.props.updateStatus} 
+            />
         )
     }
 }
@@ -44,7 +59,7 @@ const mapStateToProps = (state) => {
 };
 
 export default compose(
-    connect(mapStateToProps, { addPost, getUserProfile, getStatus, updateStatus }),
+    connect(mapStateToProps, { addPost, getUserProfile, getStatus, updateStatus, savePhoto }),
     withRouter,
     withAuthRedirect
 )(ProfileContainer);
