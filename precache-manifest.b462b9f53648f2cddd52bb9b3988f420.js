3a719135f3d610941c98f81859e8efaf72a58c5d
vlrki <valera.kirichenko@gmail.com>
2020-03-19 16:24:36 +0500

    Updates

---
+++ precache-manifest.b462b9f53648f2cddd52bb9b3988f420.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41b5f3da46136bf80fbf3f5fd33433ed",
+    "url": "/training-react-social-network/index.html"
+  },
+  {
+    "revision": "d9e6f439e376ece91e7e",
+    "url": "/training-react-social-network/static/css/3.f93178fb.chunk.css"
+  },
+  {
+    "revision": "4f547ca6ebb69deb6ad1",
+    "url": "/training-react-social-network/static/css/4.a8186abd.chunk.css"
+  },
+  {
+    "revision": "6a5e39ad466476b375c1",
+    "url": "/training-react-social-network/static/css/5.27017000.chunk.css"
+  },
+  {
+    "revision": "c83b926563d78fcb1e58",
+    "url": "/training-react-social-network/static/css/6.0e57522c.chunk.css"
+  },
+  {
+    "revision": "7c41f2263ed656afad48",
+    "url": "/training-react-social-network/static/css/main.db0bdcc8.chunk.css"
+  },
+  {
+    "revision": "fe33d2e959891db68269",
+    "url": "/training-react-social-network/static/js/0.258aeed0.chunk.js"
+  },
+  {
+    "revision": "7ac117a28401fa08f25a",
+    "url": "/training-react-social-network/static/js/10.ec9f57c6.chunk.js"
+  },
+  {
+    "revision": "d9e6f439e376ece91e7e",
+    "url": "/training-react-social-network/static/js/3.97f8cb69.chunk.js"
+  },
+  {
+    "revision": "4cbe2726fb66bcef66051a0d56105b42",
+    "url": "/training-react-social-network/static/js/3.97f8cb69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f547ca6ebb69deb6ad1",
+    "url": "/training-react-social-network/static/js/4.2635f36a.chunk.js"
+  },
+  {
+    "revision": "6a5e39ad466476b375c1",
+    "url": "/training-react-social-network/static/js/5.05b78ebf.chunk.js"
+  },
+  {
+    "revision": "c83b926563d78fcb1e58",
+    "url": "/training-react-social-network/static/js/6.17b871ad.chunk.js"
+  },
+  {
+    "revision": "a497a0cf216c50219d5e",
+    "url": "/training-react-social-network/static/js/7.0e379a78.chunk.js"
+  },
+  {
+    "revision": "1b48f795f31eb6d7757f",
+    "url": "/training-react-social-network/static/js/8.03e7c351.chunk.js"
+  },
+  {
+    "revision": "6d368532fa1a716ed460",
+    "url": "/training-react-social-network/static/js/9.a2af0969.chunk.js"
+  },
+  {
+    "revision": "7c41f2263ed656afad48",
+    "url": "/training-react-social-network/static/js/main.70f7cfd5.chunk.js"
+  },
+  {
+    "revision": "69846f757551679aee70",
+    "url": "/training-react-social-network/static/js/runtime-main.a01f9845.js"
+  },
+  {
+    "revision": "447449d2db4720b1c93a4337ee09d941",
+    "url": "/training-react-social-network/static/media/preloader.447449d2.svg"
+  }
+]);
